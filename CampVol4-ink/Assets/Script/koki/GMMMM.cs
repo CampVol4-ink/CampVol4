842a33ed3236d005f6da336d8cb55ebc73379d90
hiroki <nagadon0424@icloud.com>
2022-09-15 17:28:12 +0900

    add Ending

--- CampVol4-ink/Assets/Script/koki/GMMMM.cs
+++ CampVol4-ink/Assets/Script/koki/GMMMM.cs
@@ -1,20 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class GMMMM : MonoBehaviour{
     [SerializeField] GameObject GCUI;
     [SerializeField] private GhostStatus status;
+    [SerializeField] private GameObject ending;
+    private VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        videoPlayer = ending.GetComponent<VideoPlayer>();
+    }
+
     void Update(){
         //goalsの処理
         if(status.hp<=0){
-            Invoke("ShowGoalUI", 0.1f);
+            ending.SetActive(true);
+            videoPlayer.Play();
+            Invoke("ShowGoalUI", 10f);
         }
     }
     private void ShowGoalUI()
     {
+        videoPlayer.Stop();
         GCUI.SetActive(true);
     }
 }
